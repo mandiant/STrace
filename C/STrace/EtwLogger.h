f4aff533a5cd066930017030e008b1c3d2ae10ae
Alex Kerr <kirby@cpan.org>
2023-07-14 03:06:31 +0100

    Something that at least writes events

--- C/STrace/EtwLogger.h
+++ C/STrace/EtwLogger.h
@@ -4,7 +4,47 @@
 namespace detail
 {
 
-NTSTATUS EtwCreateTracePropertyRecursive(OUT EVENT_DATA_DESCRIPTOR fields[], int current)
+struct ProviderMetadata
+{
+	uint16_t TotalLength;
+	char ProviderName[ANYSIZE_ARRAY];
+};
+
+__declspec(noinline) NTSTATUS CreateProviderMetadata(REGHANDLE regHandle, const char* providerName, OUT EVENT_DATA_DESCRIPTOR& providerMetadataDesc)
+{
+	// Create packaged provider metadata structure.
+	// <https://learn.microsoft.com/en-us/windows/win32/etw/provider-traits>
+	const auto providerMetadataLength = (uint16_t)((strlen(providerName) + 1) + sizeof(uint16_t));
+	const auto providerMetadata = (struct detail::ProviderMetadata*)ExAllocatePoolWithTag(NonPagedPoolNx, providerMetadataLength, 'wteO');
+	RtlSecureZeroMemory(providerMetadata, providerMetadataLength);
+	providerMetadata->TotalLength = providerMetadataLength;
+	strcpy(providerMetadata->ProviderName, providerName);
+
+	// Create an EVENT_DATA_DESCRIPTOR pointing to the metadata.
+	EventDataDescCreate(&providerMetadataDesc, providerMetadata, providerMetadata->TotalLength);
+	providerMetadataDesc.Type = EVENT_DATA_DESCRIPTOR_TYPE_PROVIDER_METADATA;  // Descriptor contains provider metadata.
+
+	// Set the metadata on the provider.
+	const auto status = EtwSetInformation(regHandle, EventProviderSetTraits, providerMetadata, providerMetadata->TotalLength);
+
+	// TODO: De-allocate the original copy (???)
+	//ExFreePool(providerMetadata);
+
+	// Do we need to call EtwSetInformation at all? Or just stick the
+	// EVENT_DATA_DESCRIPTOR at the front of the list passed to EtwWriteEvent
+	// (along with another one for the event metadata?). DTrace source does have a
+	// call to the (userspace equivalent of) EtwSetInformation, but is it called?
+	// Whereas this file (<https://github.com/billti/cpp-etw/blob/master/etw-provider.h>)
+	// references the function but does not actually call it, and just sticks the
+	// descriptors in front of the write.
+	//
+	// Note that what is sent to EtwSetInformation is NOT a data descriptor, it's
+	// the plain format.
+
+	return status;
+}
+
+__declspec(noinline) NTSTATUS EtwCreateTracePropertyRecursive(OUT EVENT_DATA_DESCRIPTOR fields[], int current)
 {
 	UNREFERENCED_PARAMETER(fields);
 	UNREFERENCED_PARAMETER(current);
@@ -12,7 +52,7 @@ NTSTATUS EtwCreateTracePropertyRecursive(OUT EVENT_DATA_DESCRIPTOR fields[], int
 }
 
 template<typename FieldName = const char*, typename FieldType = int, typename FieldValue, typename... Rest>
-NTSTATUS EtwCreateTracePropertyRecursive(OUT EVENT_DATA_DESCRIPTOR fields[], int current, FieldName fieldName, FieldType fieldType, FieldValue fieldValue, Rest... rest)
+__declspec(noinline) NTSTATUS EtwCreateTracePropertyRecursive(OUT EVENT_DATA_DESCRIPTOR fields[], int current, FieldName fieldName, FieldType fieldType, FieldValue fieldValue, Rest... rest)
 {
 	// use fieldName, fieldType and fieldValue to add to fields
 	UNREFERENCED_PARAMETER(fieldName);
@@ -23,12 +63,6 @@ NTSTATUS EtwCreateTracePropertyRecursive(OUT EVENT_DATA_DESCRIPTOR fields[], int
 	// Add the next triplet of name, type and value to the event fields.
 	return EtwCreateTracePropertyRecursive(fields, current + 1, rest...);
 }
-
-struct ProviderMetadata
-{
-	uint16_t TotalLength;
-	char ProviderName[ANYSIZE_ARRAY];
-};
 
 }  // namespace detail
 
@@ -60,40 +94,45 @@ NTSTATUS EtwTrace(
 
 	// Set the name and other metadata about the provider.
 	EVENT_DATA_DESCRIPTOR providerMetadataDesc;
-	const auto providerMetadataLength = (uint16_t)(strlen(providerName) + sizeof(uint16_t));
-	const auto providerMetadata = (struct detail::ProviderMetadata*)ExAllocatePoolWithTag(NonPagedPoolNx, providerMetadataLength, 'wteO');
-	providerMetadata->TotalLength = providerMetadataLength;
-	strcpy(providerMetadata->ProviderName, providerName);
-	EventDataDescCreate(&providerMetadataDesc, providerMetadata, providerMetadata->TotalLength);
-	providerMetadataDesc.Type = 2;  // Descriptor contains provider metadata.
-	status = EtwSetInformation(regHandle, EventProviderSetTraits, providerMetadata, 1234);
-
-	// Create the collection of parameters.
-	const auto numberOfFields = sizeof...(Arguments) / 3;
-	const auto allocSize = numberOfFields * sizeof(EVENT_DATA_DESCRIPTOR);
-	const auto fields = (PEVENT_DATA_DESCRIPTOR)ExAllocatePoolWithTag(NonPagedPoolNx, allocSize, 'wteP');
-	status = detail::EtwCreateTracePropertyRecursive(OUT fields, 0, args...);
+	status = detail::CreateProviderMetadata(regHandle, providerName, OUT providerMetadataDesc);
 	if (status != STATUS_SUCCESS)
 	{
-		goto error;
+		//EtwUnregister(regHandle);
+		return status;
+	}
+
+	// Create the collection of parameters, with additional space for the metadata
+	// descriptors at the front.
+	constexpr auto numberOfFields = sizeof...(Arguments) / 3;
+	constexpr auto numberOfDescriptors = numberOfFields + 1;
+	constexpr auto allocSize = numberOfDescriptors * sizeof(EVENT_DATA_DESCRIPTOR);
+	const auto fields = (PEVENT_DATA_DESCRIPTOR)ExAllocatePoolWithTag(NonPagedPoolNx, allocSize, 'wteP');
+	RtlSecureZeroMemory(fields, allocSize);
+	memcpy(&fields[0], &providerMetadataDesc, sizeof(EVENT_DATA_DESCRIPTOR));
+	status = detail::EtwCreateTracePropertyRecursive(OUT fields, 1, args...);
+	if (status != STATUS_SUCCESS)
+	{
+		//EtwUnregister(regHandle);
+		ExFreePool(fields);
+		return status;
 	}
 
 	// Create the event descriptor.
 	EVENT_DESCRIPTOR desc;
+	RtlSecureZeroMemory(&desc, sizeof(EVENT_DESCRIPTOR));
 	desc.Level = (UCHAR)(eventLevel & 0xFF);
 
 	// Write the event.
-	status = EtwWrite(regHandle, &desc, NULL, numberOfFields, fields);
+	status = EtwWrite(regHandle, &desc, NULL, numberOfDescriptors, fields);
 	if (status != STATUS_SUCCESS)
 	{
-		goto error;
+		//EtwUnregister(regHandle);
+		ExFreePool(fields);
+		return status;
 	}
 
-error:
-	// Unregister the kernel-mode ETW provider.
-	EtwUnregister(regHandle);
-
-	ExFreePool(providerMetadata);
+	//TODO: EtwUnregister(regHandle);
 	ExFreePool(fields);
+
 	return status;
 }
